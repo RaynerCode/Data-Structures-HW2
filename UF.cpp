c6a36d6a28e42b7e4c1b0752f813e7dd7cad384b
Roie Engelstein <roiee@campus.technion.ac.il>
2026-01-19 21:04:51 +0200

    split UF into h and cpp files. added method implementation

---
+++ UF.cpp
@@ -0,0 +1,123 @@
+#include "UF.h"
+
+//public methods:
+
+UF::UF(): parent(), item_data(), Set_data() {}
+
+void UF::MakeSet(int key, std::shared_ptr<Hunter> item, std::shared_ptr<Squad> Set){
+  parent.insert(key, -1); //-1 indicates that it is the root of the set.
+  item_data.insert (key, hunterNode(item));
+  Set_data.insert(key, Set);
+}
+
+void UF::AddToSet(int key, std::shared_ptr<Hunter> item, int group_mermber_key){
+  Find(group_mermber_key); //Finds the set to add to
+  MakeSet(key, item, nullptr); //create an imaginary set
+  Union(key, group_mermber_key); 
+  /*unite the imaginary set with the real one.
+  /Note that the imaginary set's squad is the same as the real one's
+  /so it has equal member count. this means the imagenary set will always be
+  /united into the real one, keeping the real one's root.
+  */
+}
+
+void UF::Union(int key1, int key2){
+  int parent_key1 = FindRoot(key1);
+  int parent_key2 = FindRoot(key2);
+
+  if(Set_data.getValue(parent_key1) == nullptr || Set_data.getValue(parent_key2) == nullptr)
+    throw std::runtime_error("one or more sets do not exist");
+
+  if(parent_key1 == parent_key2) return; //already in the same set.
+
+  Squad& set1 = *(Set_data.getValue(parent_key1).get());
+  Squad& set2 = *(Set_data.getValue(parent_key2).get());
+
+  //Get Nodes
+  hunterNode node1 = item_data.getValue(parent_key1);
+  hunterNode node2 = item_data.getValue(parent_key2);
+
+  if(set1.get_hunter_count() >= set2.get_hunter_count()){
+    //Update root
+    parent.setData(parent_key2, parent_key1);
+
+    //Update nen
+    node2.hunter_nen += (node1.sub_group_nen - node1.hunter_nen);
+    node1.sub_group_nen += node2.sub_group_nen;
+
+    //Update fights had
+    node2.hunter->SetFightHad(node2.hunter->GetFightsHad() - node1.hunter->GetFightsHad());
+
+    //Update sets
+    Set_data.setData(parent_key2, nullptr);
+  }
+  else{
+    //Update root
+    parent.setData(parent_key1, parent_key2);
+
+    //Update nen
+    node2.hunter_nen += node1.sub_group_nen;
+    node1.hunter_nen -= node2.hunter_nen;
+
+    //Update fights had
+    node1.hunter->SetFightHad(node1.hunter->GetFightsHad() - node2.hunter->GetFightsHad());
+
+    //Update sets
+    Set_data.setData(parent_key1, nullptr);
+  }
+}
+
+std::shared_ptr<Squad> UF::Find(int key){
+  return Set_data.getValue(FindRoot(key));
+}
+
+int UF::GetHunterFightsHad(int key){
+  int root = FindRoot(key); //also compresses path
+  return item_data.getValue(key).hunter->GetFightsHad() +
+    item_data.getValue(root).hunter->GetFightsHad();
+}
+
+NenAbility UF::GetPartialNen(int key){
+  int root = FindRoot(key); //also compresses path
+  NenAbility nen(item_data.getValue(key).hunter_nen);
+  nen += item_data.getValue(root).hunter_nen; 
+  return nen;
+}
+
+//private helper methods:
+
+void UF::PathCompress(Stack<int>& path){
+  if(path.IsEmpty()) return;
+  
+  int parent_key = parent.getValue(path.Pop());
+  int fights_to_add = item_data.getValue(parent_key).hunter->GetFightsHad();
+  NenAbility partial_nen(item_data.getValue(parent_key).hunter_nen);
+  while(!path.IsEmpty()){
+    //get hunter to update
+    parent_key = path.Pop();
+    Hunter& hunter = *(item_data.getValue(parent_key).hunter);
+
+    //update fights had
+    int temp = hunter.GetFightsHad();
+    hunter.SetFightHad(hunter.GetFightsHad() + fights_to_add);
+    fights_to_add += temp;
+
+    //update partial nen
+    item_data.getValue(parent_key).hunter_nen += partial_nen;
+    partial_nen = item_data.getValue(parent_key).hunter_nen;
+
+    //update parent
+    parent.setData(path.Pop(), parent_key);
+  }
+}
+
+int UF::FindRoot(int key){
+  Stack<int> path;
+  int parent_key = key;
+  while(parent.getValue(parent_key) != -1){
+    path.Push(parent_key);
+    parent_key = parent.getValue(parent_key);
+  }
+  PathCompress(path);
+  return parent_key;
+}
--- UF.h
+++ UF.h
@@ -5,17 +5,30 @@
 
 class Hunter{
   public:
+  Hunter(Hunter& hunter) = default;
   void SetFightHad(int fights);
   int GetFightsHad() const;
-  NenAbility& GetNenAbility();
+  NenAbility& GetNenAbility() const;
 };
-class Squad;
+class Squad{
+  public:
+  int get_hunter_count() const;
+};
 
 class UF{
+  struct hunterNode{
+    std::shared_ptr<Hunter> hunter;
+    NenAbility sub_group_nen;
+    NenAbility hunter_nen;
+
+    hunterNode(std::shared_ptr<Hunter> hunter)
+     : hunter(hunter), sub_group_nen(hunter->GetNenAbility()), hunter_nen(hunter->GetNenAbility()) {}
+  };
+
   hashedArray<int> parent;
-  hashedArray<std::shared_ptr<Hunter>> item_data;
+  hashedArray<hunterNode> item_data;
   hashedArray<std::shared_ptr<Squad>> Set_data;
-  hashedArray<NenAbility> partial_nen_ability;
+
   public:
 
     UF();
@@ -36,7 +49,7 @@ class UF{
     int GetHunterFightsHad(int key);
 
     //returns the sum of nen ability of all hunters in the set up to key.
-    NenAbility& GetPartialNenAbility(int key);
+    NenAbility GetPartialNen(int key);
 
   private: //helper functions
 
@@ -46,107 +59,5 @@ class UF{
   */
   void PathCompress(Stack<int>& path);
 
-  //Updates the roots of two sets after union.
-  void UpdateRoot(int root_key1, int root_key2);
+  int FindRoot(int key);
 };
-
-//public methods:
-
-UF::UF(): parent(), item_data(), Set_data() {}
-
-void UF::MakeSet(int key, std::shared_ptr<Hunter> item, std::shared_ptr<Squad> Set){
-  parent.insert(key, -1); //-1 indicates that it is the root of the set.
-  item_data.insert(key, item);
-  Set_data.insert(key, Set);
-}
-
-void UF::AddToSet(int key, std::shared_ptr<Hunter> item, int group_mermber_key){
-  int parant_key = parent.getValue(group_mermber_key);
-  while (parant_key != -1)
-    parant_key = parent.getValue(parant_key);
-  parent.insert(key, parant_key);
-  item_data.insert(key, item);
-  Set_data.insert(key, nullptr); //Set data is only stored at the root.
-
-  item.get()->SetFightHad(item.get()->GetFightsHad() - item_data.getValue(parant_key).get()->GetFightsHad());
-}
-
-void UF::Union(int key1, int key2){
-  Stack<int> path1, path2;
-  int parant_key1 = key1, parant_key2 = key2;
-
-
-  while(parent.getValue(parant_key1) != -1){
-    path1.Push(parant_key1);
-    parant_key1 = parent.getValue(parant_key1);
-  }
-
-  PathCompress(path1);
-
-  while(parent.getValue(parant_key2) != -1){
-    path2.Push(parant_key2);
-    parant_key2 = parent.getValue(parant_key2);
-  }
-
-  if(parant_key1 == parant_key2) return; //already in the same set.
-
-  PathCompress(path2); //If we recived the same set twice, can't gurentee path2's validity.
-
-  UpdateRoot(parant_key1, parant_key2);
-}
-
-std::shared_ptr<Squad> UF::Find(int key){
-  if(parent.getValue(key) == -1) return Set_data.getValue(key);
-
-  Stack<int> path;
-  int parant_key = key;
-  while(parent.getValue(parant_key) != -1){
-    path.Push(parant_key);
-    parant_key = parent.getValue(parant_key);
-  }
-
-  PathCompress(path);
-
-  return Set_data.getValue(parant_key);
-}
-
-int UF::GetHunterFightsHad(int key){
-  if(parent.getValue(key) == -1) return item_data.getValue(key).get()->GetFightsHad();
-
-  Stack<int> path;
-  int parant_key = key;
-  int total_fights = item_data.getValue(key).get()->GetFightsHad();
-  while(parent.getValue(parant_key) != -1){
-    path.Push(parant_key);
-    parant_key = parent.getValue(parant_key);
-    total_fights += item_data.getValue(parant_key).get()->GetFightsHad();
-  }
-
-  PathCompress(path);
-
-  return total_fights;
-}
-
-NenAbility& UF::GetPartialNenAbility(int key){
-  //ToDo
-}
-
-//private helper methods:
-
-void UF::PathCompress(Stack<int>& path){
-  if(path.IsEmpty()) return;
-  
-  int parant_key = parent.getValue(path.Peek());
-  int fights_to_add = item_data.getValue(path.Pop()).get()->GetFightsHad();
-  while(!path.IsEmpty()){
-    Hunter& hunter = *(item_data.getValue(path.Peek()).get());
-    int temp = hunter.GetFightsHad();
-    hunter.SetFightHad(hunter.GetFightsHad() + fights_to_add);
-    fights_to_add += temp;
-    parent.SetData(path.Pop(), parant_key);
-  }
-}
-
-void UF::UpdateRoot(int root_key1, int root_key2){
-
-}
