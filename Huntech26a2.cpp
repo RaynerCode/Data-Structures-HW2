cf1b553a31c56847e1f7a8099e13b4700befbfed
RaynerCode <etamar375@gmail.com>
2026-01-22 12:24:26 +0200

    add huntech

---
+++ Huntech26a2.cpp
@@ -0,0 +1,73 @@
+// You can edit anything you want in this file.
+// However, you need to implement all public Huntech functions, which are provided below as a template.
+
+#include "Huntech26a2.h"
+
+Huntech::Huntech() = default;
+
+Huntech::~Huntech() = default;
+
+StatusType Huntech::add_squad(int squadId) {
+    //do squadTree.Add(squadId)
+    if(squadId <= 0) {
+        return StatusType::INVALID_INPUT;
+    }
+    try {
+        this->squadIdTree.Add(Squad(squadId));
+    }
+
+    return StatusType::FAILURE;
+}
+
+StatusType Huntech::remove_squad(int squadId) {
+    //remove squad from Tree. the UF isn't changed because this squad becomes numb, it can't be
+    //united with any other squad because union happens through SquadIds.
+    return StatusType::FAILURE;
+}
+
+StatusType Huntech::add_hunter(int hunterId,
+                               int squadId,
+                               const NenAbility &nenType,
+                               int aura,
+                               int fightsHad)
+{
+    //find squadId in tree, if exists with zero hunters, add and change initialHunterId field
+    //if it had hunters, go to the UF.find(initialHunterId) and UF.AddToSet the new hunter
+    //if it had no hunters, do a UF.makeSet(new hunter, found squad);
+    return StatusType::FAILURE;
+}
+
+output_t<int> Huntech::squad_duel(int squadId1, int squadId2) {
+    //find both squads in SquadTree, compare in O(1). use Squad.addSquadExp accordingly.
+    return 0;
+}
+
+output_t<int> Huntech::get_hunter_fights_number(int hunterId) {
+    //do UF.GetNumberFightsHad(hunterId)
+    return 0;
+}
+
+output_t<int> Huntech::get_squad_experience(int squadId) {
+    //find squad in squadTree and return squad.GetSquadExp;
+    return 0;
+}
+
+output_t<int> Huntech::get_ith_collective_aura_squad(int i) {
+    //return Get_ithRank(i).GetSquadId() in a try catch.
+    return 0;
+}
+
+output_t<NenAbility> Huntech::get_partial_nen_ability(int hunterId) {
+    //return UF.GetPartialNen(id);
+    return NenAbility();
+}
+
+StatusType Huntech::force_join(int forcingSquadId, int forcedSquadId) {
+    /* check if the forcing is allowed (RAPE!)
+     * set forcingHunter = forcing.GetInitialHunterId(), forcedHunter = forced.GetInitialHunterId();
+     * do UF.union(forcingHunter,forcedHunter);
+     * do forcing.merge(forced) in the squad Tree
+     * remove forcedSquad from Tree using forcedSquadId
+     */
+    return StatusType::FAILURE;
+}
---
+++ Huntech26a2.h
@@ -0,0 +1,62 @@
+// 
+// 234218 Data Structures 1.
+// Semester: 2026A (Winter).
+// Wet Exercise #2 - Huntech.
+// 
+// The following header file contains all methods we expect you to implement.
+// You MAY add private methods and fields of your own.
+// DO NOT erase or modify the signatures of the public methods.
+// DO NOT modify the preprocessors in this file.
+// DO NOT use the preprocessors in your other code files.
+// 
+
+#ifndef HUNTECH26A2_H_
+#define HUNTECH26A2_H_
+#pragma once
+#include "wet2util.h"
+#include "UF.h"
+#include "RankTree.h"
+
+
+class Huntech {
+private:
+    //
+    // Here you may add anything you need to implement your Huntech class
+    //
+    UF m_uf;
+    RankTree<Squad> squadAuraTree;
+    AvlTree<Squad> squadIdTree;
+    //squadAuraTree sorts squads by their aura first and then by their id. each node should hold the number of squads with less
+
+public:
+    // <DO-NOT-MODIFY> {
+
+    Huntech();
+    virtual ~Huntech();
+
+    StatusType add_squad(int squadId);
+    
+    StatusType remove_squad(int squadId);
+    
+    StatusType add_hunter(int hunterId,
+                          int squadId,
+                          const NenAbility &nenType,
+                          int aura,
+                          int fightsHad);
+
+    output_t<int> squad_duel(int squadId1, int squadId2);
+
+    output_t<int> get_hunter_fights_number(int hunterId);
+
+    output_t<int> get_squad_experience(int squadId);
+
+    output_t<int> get_ith_collective_aura_squad(int i);
+
+    output_t<NenAbility> get_partial_nen_ability(int hunterId);
+
+    StatusType force_join(int forcingSquadId, int forcedSquadId);
+
+    // } </DO-NOT-MODIFY>
+};
+
+#endif // HUNTECH26A2_H_
